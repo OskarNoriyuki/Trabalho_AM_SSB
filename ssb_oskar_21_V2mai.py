02d5bdea68a52e807c74f5429f02da1263690c89
OskarNoriyuki <57155385+OskarNoriyuki@users.noreply.github.com>
2021-05-21 02:44:24 -0300

    ssb weaver demod

---
+++ ssb_oskar_21_V2mai.py
@@ -0,0 +1,426 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: ssb_oskar_21_V2mai
+# Author: oskar
+# GNU Radio version: v3.8.2.0-57-gd71cd177
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import channels
+from gnuradio import filter
+from gnuradio import gr
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio.qtgui import Range, RangeWidget
+
+from gnuradio import qtgui
+
+class ssb_oskar_21_V2mai(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "ssb_oskar_21_V2mai")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("ssb_oskar_21_V2mai")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "ssb_oskar_21_V2mai")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 2000E3
+        self.input_gain = input_gain = 1
+        self.f_error = f_error = -0.5
+        self.carrier_freq = carrier_freq = 500E3
+        self.bandwidth = bandwidth = 1000000
+        self.agc_decay = agc_decay = 65e-06
+        self.agc_attack = agc_attack = 0.1
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._agc_decay_range = Range(10e-06, 100e-06, 1e-06, 65e-06, 200)
+        self._agc_decay_win = RangeWidget(self._agc_decay_range, self.set_agc_decay, 'AGC Decay', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._agc_decay_win)
+        self._agc_attack_range = Range(0.1, 0.3, 0.001, 0.1, 200)
+        self._agc_attack_win = RangeWidget(self._agc_attack_range, self.set_agc_attack, 'AGC Attack', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._agc_attack_win)
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_fff(
+                interpolation=int(44100/100),
+                decimation=int(carrier_freq/100),
+                taps=None,
+                fractional_bw=None)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=int(carrier_freq/100),
+                decimation=int(44100/100),
+                taps=None,
+                fractional_bw=0.4)
+        self.qtgui_waterfall_sink_x_2 = qtgui.waterfall_sink_f(
+            1024, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "TX_in", #name
+            1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_2.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_2.enable_grid(False)
+        self.qtgui_waterfall_sink_x_2.enable_axis_labels(True)
+
+
+        self.qtgui_waterfall_sink_x_2.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_2.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_2.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_2.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_2.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_2.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_2_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_2.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_2_win)
+        self.qtgui_waterfall_sink_x_1 = qtgui.waterfall_sink_f(
+            1024, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "mixer_out", #name
+            1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_1.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_1.enable_grid(False)
+        self.qtgui_waterfall_sink_x_1.enable_axis_labels(True)
+
+
+        self.qtgui_waterfall_sink_x_1.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_1.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_1.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_1.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_1.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_1.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_1_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_1.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_1_win)
+        self.qtgui_waterfall_sink_x_0_0 = qtgui.waterfall_sink_f(
+            1024, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            44100, #bw
+            "receptor", #name
+            1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0_0.enable_axis_labels(True)
+
+
+        self.qtgui_waterfall_sink_x_0_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_0_win)
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_f(
+            1024, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+
+
+        self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(
+            1,
+            firdes.low_pass(
+                1,
+                samp_rate*(1+f_error),
+                20000,
+                6000,
+                firdes.WIN_HAMMING,
+                6.76))
+        self.hilbert_fc_0 = filter.hilbert_fc(65, firdes.WIN_HAMMING, 6.76)
+        self.channels_channel_model_0 = channels.channel_model(
+            noise_voltage=0.1,
+            frequency_offset=1,
+            epsilon=1.0,
+            taps=[1.0 + 1.0j],
+            noise_seed=0,
+            block_tags=False)
+        self.blocks_wavfile_source_0 = blocks.wavfile_source('C:\\Program Files\\GNURadio-3.8\\bin\\wav_music.wav', False)
+        self.blocks_throttle_0_0_0 = blocks.throttle(gr.sizeof_float*1, 44.1e3,True)
+        self.blocks_multiply_xx_1 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0_0 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_ff(1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(input_gain)
+        self.blocks_float_to_complex_2 = blocks.float_to_complex(1)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_complex_to_float_2 = blocks.complex_to_float(1)
+        self.blocks_complex_to_float_1 = blocks.complex_to_float(1)
+        self.blocks_complex_to_float_0_0 = blocks.complex_to_float(1)
+        self.blocks_add_xx_0 = blocks.add_vff(1)
+        self.band_pass_filter_0 = filter.fir_filter_fff(
+            1,
+            firdes.band_pass(
+                1,
+                samp_rate,
+                carrier_freq+500,
+                carrier_freq+20500,
+                4000,
+                firdes.WIN_HAMMING,
+                6.76))
+        self.audio_sink_0_0 = audio.sink(44100, '', True)
+        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate*(1+f_error), analog.GR_COS_WAVE, carrier_freq*(1+f_error), .5, 0, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate*(1+f_error), analog.GR_SIN_WAVE, carrier_freq*(1+f_error), .5, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, carrier_freq, 1, 0, 0)
+        self.analog_agc2_xx_0 = analog.agc2_ff(agc_attack, agc_decay, .5, 1)
+        self.analog_agc2_xx_0.set_max_gain(1)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_agc2_xx_0, 0), (self.blocks_multiply_xx_0_0, 1))
+        self.connect((self.analog_agc2_xx_0, 0), (self.blocks_multiply_xx_1, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.hilbert_fc_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_float_to_complex_2, 0))
+        self.connect((self.blocks_complex_to_float_0_0, 0), (self.blocks_multiply_xx_1, 1))
+        self.connect((self.blocks_complex_to_float_1, 0), (self.rational_resampler_xxx_0_0, 0))
+        self.connect((self.blocks_complex_to_float_2, 0), (self.analog_agc2_xx_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.channels_channel_model_0, 0))
+        self.connect((self.blocks_float_to_complex_2, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.audio_sink_0_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.qtgui_waterfall_sink_x_0_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.band_pass_filter_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_waterfall_sink_x_1, 0))
+        self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.blocks_throttle_0_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_throttle_0_0_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.blocks_complex_to_float_2, 0))
+        self.connect((self.hilbert_fc_0, 0), (self.blocks_complex_to_float_0_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_complex_to_float_1, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_waterfall_sink_x_2, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.blocks_multiply_const_vxx_1, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "ssb_oskar_21_V2mai")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate*(1+self.f_error))
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate*(1+self.f_error))
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, self.carrier_freq+500, self.carrier_freq+20500, 4000, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate*(1+self.f_error), 20000, 6000, firdes.WIN_HAMMING, 6.76))
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_waterfall_sink_x_1.set_frequency_range(0, self.samp_rate)
+        self.qtgui_waterfall_sink_x_2.set_frequency_range(0, self.samp_rate)
+
+    def get_input_gain(self):
+        return self.input_gain
+
+    def set_input_gain(self, input_gain):
+        self.input_gain = input_gain
+        self.blocks_multiply_const_vxx_0.set_k(self.input_gain)
+
+    def get_f_error(self):
+        return self.f_error
+
+    def set_f_error(self, f_error):
+        self.f_error = f_error
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate*(1+self.f_error))
+        self.analog_sig_source_x_0_0.set_frequency(self.carrier_freq*(1+self.f_error))
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate*(1+self.f_error))
+        self.analog_sig_source_x_1.set_frequency(self.carrier_freq*(1+self.f_error))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate*(1+self.f_error), 20000, 6000, firdes.WIN_HAMMING, 6.76))
+
+    def get_carrier_freq(self):
+        return self.carrier_freq
+
+    def set_carrier_freq(self, carrier_freq):
+        self.carrier_freq = carrier_freq
+        self.analog_sig_source_x_0.set_frequency(self.carrier_freq)
+        self.analog_sig_source_x_0_0.set_frequency(self.carrier_freq*(1+self.f_error))
+        self.analog_sig_source_x_1.set_frequency(self.carrier_freq*(1+self.f_error))
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, self.carrier_freq+500, self.carrier_freq+20500, 4000, firdes.WIN_HAMMING, 6.76))
+
+    def get_bandwidth(self):
+        return self.bandwidth
+
+    def set_bandwidth(self, bandwidth):
+        self.bandwidth = bandwidth
+
+    def get_agc_decay(self):
+        return self.agc_decay
+
+    def set_agc_decay(self, agc_decay):
+        self.agc_decay = agc_decay
+        self.analog_agc2_xx_0.set_decay_rate(self.agc_decay)
+
+    def get_agc_attack(self):
+        return self.agc_attack
+
+    def set_agc_attack(self, agc_attack):
+        self.agc_attack = agc_attack
+        self.analog_agc2_xx_0.set_attack_rate(self.agc_attack)
+
+
+
+
+
+def main(top_block_cls=ssb_oskar_21_V2mai, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
